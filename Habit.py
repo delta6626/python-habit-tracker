f95b8316f8d4830c85e809dae4a82899a1472d48
Delta6626 <hassan911ee@gmail.com>
2026-08-29 23:14:21 +0400

    feat: modify Habit constructor to accept optional id parameter

--- Habit.py
+++ Habit.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from constants import Periodicity, PERIODICITY_DAY_COUNT
 
 class Habit:
-    def __init__(self, name:str, description:str, periodicity:Periodicity, created_at:datetime, completions:list[datetime]):
-        self.id = str(uuid4())
+    def __init__(self, id:str | None, name:str, description:str, periodicity:Periodicity, created_at:datetime, completions:list[datetime]):
+        self.id = id or str(uuid4())
         self.name = name
         self.description = description
         self.periodicity = periodicity
--- HabitManager.py
+++ HabitManager.py
@@ -2,21 +2,19 @@ from HabitDatabase import HabitDatabase
 from datetime import datetime
 from Habit import Habit
 from utilities import get_non_empty_input, get_periodicity_input
-import analytics
 
 
 class HabitManager:
     def __init__(self):
         self.database = HabitDatabase()
+        self.habit_list = self.database.get_all_habits();
 
     def add_new_habit(self):
         habit_name = get_non_empty_input("Habit name", "Enter a name for your new habit: ")
         habit_description = input("Enter a description for your new habit (optional): ").strip()
         habit_periodicity = get_periodicity_input("Enter the desired periodicity for your habit (daily/weekly): ")
 
-        new_habit = Habit(habit_name, habit_description, habit_periodicity, datetime.now(), [])
+        new_habit = Habit(None, habit_name, habit_description, habit_periodicity, datetime.now(), [])
         self.database.add_habit(new_habit)
+        self.habit_list.append(new_habit)
         print("New habit added successfully.\n")
-
-    def view_all_habits(self):
-        return analytics.view_all_habits(self.database.get_all_habits())
