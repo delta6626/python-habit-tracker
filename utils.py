e4aef5d19adea95661ce4f0b12db3dd358153871
Delta6626 <hassan911ee@gmail.com>
2026-09-19 01:05:35 +0400

    rename file

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 import json
 
-file_name = "config.json"
+config_file_name = "config.json"
 
 
 def is_demo_data_loaded() -> bool:
     try:
-        with open(file_name, "r") as file:
+        with open(config_file_name, "r") as file:
             config_data = json.load(file)
             return config_data["demo_data_loaded"]
 
     except (FileNotFoundError, KeyError):
         config_data = {"demo_data_loaded": False}
 
-        with open(file_name, "w") as file:
+        with open(config_file_name, "w") as file:
             json.dump(config_data, file)
 
         return False
@@ -20,16 +20,16 @@ def is_demo_data_loaded() -> bool:
 
 def update_demo_data_status(status: bool) -> None:
     try:
-        with open(file_name, "r") as file:
+        with open(config_file_name, "r") as file:
             config_data = json.load(file)
 
         config_data["demo_data_loaded"] = status
 
-        with open(file_name, "w") as file:
+        with open(config_file_name, "w") as file:
             json.dump(config_data, file)
 
     except (FileNotFoundError, KeyError):
         config_data = {"demo_data_loaded": status}
 
-        with open(file_name, "w") as file:
+        with open(config_file_name, "w") as file:
             json.dump(config_data, file)
