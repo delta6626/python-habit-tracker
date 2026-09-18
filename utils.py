a840c400af6f93c0320fba939b08adfa148f8f8a
Delta6626 <hassan911ee@gmail.com>
2026-09-19 00:56:32 +0400

    update naming

--- utils.py
+++ utils.py
@@ -3,14 +3,14 @@ import json
 file_name = "config.json"
 
 
-def is_test_data_loaded() -> bool:
+def is_demo_data_loaded() -> bool:
     try:
         with open(file_name, "r") as file:
             config_data = json.load(file)
-            return config_data["test_data_loaded"]
+            return config_data["demo_data_loaded"]
 
     except (FileNotFoundError, KeyError):
-        config_data = {"test_data_loaded": False}
+        config_data = {"demo_data_loaded": False}
 
         with open(file_name, "w") as file:
             json.dump(config_data, file)
@@ -18,18 +18,18 @@ def is_test_data_loaded() -> bool:
         return False
 
 
-def update_test_data_status(status: bool) -> None:
+def update_demo_data_status(status: bool) -> None:
     try:
         with open(file_name, "r") as file:
             config_data = json.load(file)
 
-        config_data["test_data_loaded"] = status
+        config_data["demo_data_loaded"] = status
 
         with open(file_name, "w") as file:
             json.dump(config_data, file)
 
     except (FileNotFoundError, KeyError):
-        config_data = {"test_data_loaded": status}
+        config_data = {"demo_data_loaded": status}
 
         with open(file_name, "w") as file:
             json.dump(config_data, file)
