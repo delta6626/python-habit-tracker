6c6b8fa00848d56dde87af67e461ecf4704649fb
Delta6626 <hassan911ee@gmail.com>
2026-09-19 22:15:31 +0400

    write tests

--- tests/test_habit.py
+++ tests/test_habit.py
@@ -1,9 +1,45 @@
 import unittest
+from datetime import datetime, timedelta
+from Habit import Habit
 
 
 class TestHabit(unittest.TestCase):
-    pass
+    def test_habit_id_is_automatically_generated(self):
+        habit = Habit(
+            None, "Test habit", "Test habit description", "daily", datetime.now(), []
+        )
 
+        self.assertIsNotNone(
+            habit.id, "Test case failed: Habit ID is NOT being generated automatically."
+        )
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_provided_habit_id_is_preserved(self):
+        habit_id = "test_id"
+        habit = Habit(
+            habit_id,
+            "Test habit",
+            "Test habit description",
+            "daily",
+            datetime.now(),
+            [],
+        )
+
+        self.assertEqual(
+            habit.id,
+            habit_id,
+            "Test case failed: Provided Habit id is NOT being preserved in Habit instance.",
+        )
+
+    def test_check_off_habit_appends_completion(self):
+        habit = Habit(
+            None, "Test habit", "Test habit description", "daily", datetime.now(), []
+        )
+
+        habit.check_off_habit(datetime.now())
+        habit.check_off_habit(datetime.now() + timedelta(days=1))
+
+        self.assertEqual(
+            len(habit.completions),
+            2,
+            "Test case failed: Completions are NOT being appended as expected.",
+        )
