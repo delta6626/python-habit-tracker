9da42426549074d350f977d9809a7c983fb21fd0
Delta6626 <hassan911ee@gmail.com>
2026-09-19 20:39:09 +0400

    refactor: update get_longest_streak_for_habit to accept optional datetime parameter

--- analytics.py
+++ analytics.py
@@ -46,8 +46,11 @@ def group_habits_based_on_periodicity(habit_list: list[Habit]) -> str:
     )
 
 
-def get_longest_streak_for_habit(habit: Habit):
-    periods = habit.get_periods_since_creation(datetime.now())
+def get_longest_streak_for_habit(
+    habit: Habit, datetime_of_check: datetime | None = None
+) -> int:
+    now = datetime_of_check or datetime.now()
+    periods = habit.get_periods_since_creation(now)
 
     completions_list = [
         int(
