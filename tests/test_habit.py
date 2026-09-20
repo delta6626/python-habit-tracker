ee7ebc4b53cb196a2541c56b398fa26bdb636d03
Delta6626 <hassan911ee@gmail.com>
2026-09-20 15:39:18 +0400

    use existing demo data for tests

--- tests/test_habit.py
+++ tests/test_habit.py
@@ -1,9 +1,14 @@
 import unittest
 from datetime import datetime, timedelta
 from Habit import Habit
+from utilities import build_demo_data
+from constants import RECORD_IDS
 
 
 class TestHabit(unittest.TestCase):
+    def setUp(self):
+        self.habits = build_demo_data()
+
     def test_habit_id_is_automatically_generated(self):
         habit = Habit(
             None, "Test habit", "Test habit description", "daily", datetime.now(), []
@@ -14,32 +19,20 @@ class TestHabit(unittest.TestCase):
         )
 
     def test_provided_habit_id_is_preserved(self):
-        habit_id = "test_id"
-        habit = Habit(
-            habit_id,
-            "Test habit",
-            "Test habit description",
-            "daily",
-            datetime.now(),
-            [],
-        )
-
+        chosen_habit = self.habits[0]
         self.assertEqual(
-            habit.id,
-            habit_id,
-            "Test case failed: Provided Habit id is NOT being preserved in Habit instance.",
+            chosen_habit.id,
+            RECORD_IDS[0],
+            "Test case failed: Provided Habit ID is not being preserved.",
         )
 
     def test_check_off_habit_appends_completion(self):
-        habit = Habit(
-            None, "Test habit", "Test habit description", "daily", datetime.now(), []
-        )
-
-        habit.check_off_habit(datetime.now())
-        habit.check_off_habit(datetime.now() + timedelta(days=1))
+        chosen_habit = self.habits[0]
+        chosen_habit.check_off_habit(datetime.now())
+        chosen_habit.check_off_habit(datetime.now() + timedelta(days=1))
 
         self.assertEqual(
-            len(habit.completions),
-            2,
-            "Test case failed: Completions are NOT being appended as expected.",
+            len(chosen_habit.completions),
+            30,
+            "Test case failed: Habit completions are not being appended.",
         )
