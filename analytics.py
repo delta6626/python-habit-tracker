8397a57e5ec2101d975716474de69fa45221bde2
Delta6626 <hassan911ee@gmail.com>
2026-09-22 02:13:10 +0400

    add docstrings for functions in analytics.py

--- analytics.py
+++ analytics.py
@@ -4,12 +4,21 @@ from constants import PERIODICITY_DAY_COUNT
 
 
 def get_all_habits(habits: list[Habit]) -> str:
+    """
+    Return a numbered list containing the names of all habits.
+    """
+
     return "\n".join(
         map(lambda item: f"{item[0]}. {item[1].name}", enumerate(habits, start=1))
     )
 
 
 def get_habit_details(habit: Habit) -> str:
+    """
+    Return the details of a habit, including all of its completion
+    records, in a readable format.
+    """
+
     completion_times = (
         "\n".join(f"  - {completion}" for completion in habit.completions)
         if habit.completions
@@ -29,6 +38,10 @@ def get_habit_details(habit: Habit) -> str:
 
 
 def group_habits_based_on_periodicity(habit_list: list[Habit]) -> str:
+    """
+    Return all habits by grouping them based on their periodicity.
+    """
+
     periodicities = ("daily", "weekly")
 
     return "\n\n".join(
@@ -49,6 +62,16 @@ def group_habits_based_on_periodicity(habit_list: list[Habit]) -> str:
 def get_longest_streak_for_habit(
     habit: Habit, datetime_of_check: datetime | None = None
 ) -> int:
+    """
+    Calculate and return the longest consecutive completion streak for the specified habit.
+
+    The habit's periodicity determines the period length. Each period is
+    marked as completed if there is at least one completion record that
+    occured during said period. A missed period (period with no completions)
+    breaks the streak, and the longest consecutive sequence of completed
+    periods is returned.
+    """
+
     now = datetime_of_check or datetime.now()
     periods = habit.get_current_period_number(now)
 
@@ -81,7 +104,15 @@ def get_longest_streak_for_habit(
     )
 
 
-def get_longest_streak_overall(habit_list: list[Habit]) -> tuple[Habit, int]:
+def get_longest_streak_overall(habit_list: list[Habit]) -> tuple[Habit | None, int]:
+    """
+    Find and return the habit with the longest streak overall.
+
+    Uses the get_longest_streak_for_habit function to find the longest
+    streak for each habit. The habit with the longest streak overall
+    is then identified and returned together with its streak length.
+    """
+
     return max(
         ((habit, get_longest_streak_for_habit(habit)) for habit in habit_list),
         key=lambda item: item[1],
